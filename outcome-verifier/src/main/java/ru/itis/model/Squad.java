8d0fd4eef65ee131f00a3d9e8e2aa6beb8071864
bbilalov <bulat.bilalov@orioninc.com>
2023-02-16 16:56:39 +0100

    Added the Squad object

---
+++ outcome-verifier/src/main/java/ru/itis/model/Squad.java
@@ -0,0 +1,22 @@
+package ru.itis.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@EqualsAndHashCode(callSuper = false)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "squads")
+public class Squad extends BaseEntity{
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "state_id", referencedColumnName = "id")
+    private State state;
+}
