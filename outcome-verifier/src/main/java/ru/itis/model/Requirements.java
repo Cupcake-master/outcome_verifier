8c58e1c964c8973399688d29fbb35ebe87739e4a
Bilalov_Bulat <programming.languages.only@gmail.com>
2023-04-19 10:24:54 +0300

    Added the new entity: Requirements

---
+++ outcome-verifier/src/main/java/ru/itis/model/Requirements.java
@@ -0,0 +1,27 @@
+package ru.itis.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@EqualsAndHashCode(callSuper = false)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "requirements")
+public class Requirements extends BaseEntity{
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "module_id", referencedColumnName = "id")
+    private Module module_id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "grade_id", referencedColumnName = "id")
+    private Grade grade_id;
+
+    private Integer requiredNumberOfTasksSolved;
+
+    private Integer requiredPercentageOfKeywordsUsed;
+}
